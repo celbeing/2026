b57ecf5f0997afa0ed7a47495117d47d15464d3d
전라남도교육지원청 <kimsd1983@naver.com>
2026-03-31 11:56:07 +0900

    BOJ 12844 XOR iterative solution

--- PS/12844 XOR(Iterative).py
+++ PS/12844 XOR(Iterative).py
@@ -1,23 +1,94 @@
+# python3: TLE, pypy3: 1500ms
+
 import sys
-input = sys.stdin.readline
 
 n = int(input())
-seg = [0] * n * 4
-lazy = [0] * n * 4
-arr = list(map(int, input().split()))
+a = list(map(int, input().split()))
 
-def init_tree(node, start, end):
-    if start == end:
-        seg[node] = arr[start - 1]
-        return
+size = 1
+while size < n: size <<= 1
 
-    mid = (start + end) // 2
-    init_tree(node*2, start, mid)
-    init_tree(node*2+1, mid+1, end)
+seg = [0] * size * 2
+lazy = [0] * size * 2
+leaf_count = [0] * size * 2
+LOG = size.bit_length() - 1
 
-    seg[node] = seg[node*2] ^ seg[node*2+1]
+for i in range(n):
+    seg[size + i] = a[i]
+    leaf_count[size + i] = 1
+for i in range(size - 1, 0, -1):
+    seg[i] = seg[i*2] ^ seg[i*2+1]
+    leaf_count[i] = leaf_count[i*2] ^ leaf_count[i*2+1]
+
+def apply(node, k):
+    if leaf_count[node]:
+        seg[node] ^= k
+    if node < size:
+        lazy[node] ^= k
     return
 
-init_tree(1, 1, n)
+def push(node):
+    for s in range(LOG, 0, -1):
+        i = node >> s
+        if lazy[i]:
+            apply(i*2, lazy[i])
+            apply(i*2+1, lazy[i])
+            lazy[i] = 0
+    return
 
-def push()
+def pull(node):
+    while node > 1:
+        node >>= 1
+        seg[node] = seg[node*2] ^ seg[node*2+1]
+        if leaf_count[node]:
+            seg[node] ^= lazy[node]
+
+def update(l, r, k):
+    l += size
+    r += size
+    l0, r0 = l, r
+
+    push(l0)
+    push(r0)
+
+    while l <= r:
+        if l & 1:
+            apply(l, k)
+            l += 1
+        if not(r & 1):
+            apply(r, k)
+            r -= 1
+        l >>= 1
+        r >>= 1
+
+    pull(l0)
+    pull(r0)
+    return
+
+def query(l, r):
+    l += size
+    r += size
+    push(l)
+    push(r)
+
+    res = 0
+    while l <= r:
+        if l & 1:
+            res ^= seg[l]
+            l += 1
+        if not(r & 1):
+            res ^= seg[r]
+            r -= 1
+        l >>= 1
+        r >>= 1
+    return res
+
+out = []
+for _ in range(int(input())):
+    line = list(map(int, input().split()))
+    if line[0] == 1:
+        update(line[1], line[2], line[3])
+    else:
+        out.append(query(line[1], line[2]))
+
+sys.stdout.write('\n'.join(map(str, out)))
--- PS/12844 XOR.py
+++ PS/12844 XOR.py
@@ -1,5 +1,4 @@
 import sys
-sys.setrecursionlimit(200000)
 input = sys.stdin.readline
 
 n = int(input())
@@ -7,53 +6,79 @@ seg = [0] * n * 4
 lazy = [0] * n * 4
 a = list(map(int, input().split()))
 
+# 트리 초기화
 def init_tree(arr, node, start, end):
+    # 리프에 도달하면 값 저장
     if start == end:
         seg[node] = arr[start-1]
         return
 
+    # 하위 노드로 이동
     mid = (start + end) // 2
     init_tree(arr, node*2, start, mid)
     init_tree(arr, node*2+1, mid+1, end)
 
+    # 현재 노드 업데이트
     seg[node] = seg[node*2] ^ seg[node*2+1]
     return
 
+# 구간 xor 쿼리
 def query(node, start, end, left, right):
+    # 갱신되지 않은 값이 있으면 push
     if lazy[node]: push(node, start, end)
+
+    # 쿼리 범위를 벗어난 경우
     if end < left or right < start:
         return 0
 
+    # 쿼리 구간에 모두 포함되는 경우
     if left <= start and end <= right:
-        return seg[node]
+        return seg[node]    # 이미 push 되어있기 때문에 모든 쿼리가 반영되어있음
 
+    # 하위 쿼리를 호출
     mid = (start + end) // 2
     return query(node*2, start, mid, left, right) ^ query(node*2+1, mid+1, end, left, right)
 
+# 구간 xor 업데이트
 def update(node, start, end, left, right, k):
+    # 갱신되지 않은 값이 있으면 push
     if lazy[node]: push(node, start, end)
+
+    # 쿼리 구간을 벗어난 경우
     if end < left or right < start:
         return
 
+    # 쿼리 구간에 모두 포함되는 경우
     if left <= start and end <= right:
+        # 리프의 수가 홀수 개인 경우만 현재 노드에 xor 반영
         if (end - start + 1) & 1:
             seg[node] ^= k
+
+        # 하위 노드가 있는 경우(리프가 아닌 경우)
+        # 하위 노드의 쿼리는 갱신하지 않고 저장
         if start < end:
             lazy[node*2] ^= k
             lazy[node*2+1] ^= k
         return
 
+    # 하위 쿼리를 호출
     mid = (start + end) // 2
     update(node*2, start, mid, left, right, k)
     update(node*2+1, mid+1, end, left, right, k)
 
+    # 현재 노드의 값 갱신
+    # 하위 쿼리를 실행했기 때문에 지연된 쿼리가 모두 반영되어있음
     seg[node] = seg[node*2] ^ seg[node*2+1]
     return
 
+# 지연된 쿼리 반영
 def push(node, start, end):
+    # 리프의 수가 홀수 개인 경우만 현재 노드에 lazy 반영
     if (end - start + 1) & 1:
         seg[node] ^= lazy[node]
 
+    # 리프가 아닌 경우
+    # 하위 노드에 lazy 값 전파
     if start < end:
         lazy[node*2] ^= lazy[node]
         lazy[node*2+1] ^= lazy[node]
