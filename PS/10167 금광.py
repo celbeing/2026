f985bc74f4e901177ecec531b96f38ac79ecdcbd
전라남도교육지원청 <76736721+celbeing@users.noreply.github.com>
2026-04-15 23:33:58 +0900

    Update 10167 금광.py

--- PS/10167 금광.py
+++ PS/10167 금광.py
@@ -23,7 +23,6 @@ y_point = [[] for _ in range(y1_point+1)]
 for x, y, w in mine:
     y_point[y_map[y]].append((x_map[x], w))
 
-# seg의 size 결정
 size = 1
 n = len(x_set)
 while size < n:
@@ -32,11 +31,10 @@ while size < n:
 result = 0
 INF = -float('inf')
 for i in range(y1_point+1):
-    # y1_point가 가리키는 y에 대해 seg 초기화
     seg = [0] * size * 2
-    left_seg = [INF] * size * 2
-    right_seg = [INF] * size * 2
-    total_seg = [INF] * size * 2
+    left_seg = [0] * size * 2
+    right_seg = [0] * size * 2
+    total_seg = [0] * size * 2
 
     for x, w in y_point[i]:
         seg[size+x] += w
@@ -47,7 +45,7 @@ for i in range(y1_point+1):
         seg[j] = seg[j*2] + seg[j*2+1]
         left_seg[j] = max(left_seg[j*2], seg[j*2] + left_seg[j*2+1])
         right_seg[j] = max(right_seg[j*2+1], right_seg[j*2] + seg[j*2+1])
-        total_seg[j] = max(max(total_seg[j*2], total_seg[j*2+1]), right_seg[j*2] + left_seg[j*2+1])
+        total_seg[j] = max(total_seg[j*2], total_seg[j*2+1], right_seg[j*2] + left_seg[j*2+1])
     result = max(result, total_seg[1])
     for j in range(i+1,y1_point+1):
         for x, w in y_point[j]:
@@ -55,11 +53,13 @@ for i in range(y1_point+1):
             left_seg[size+x] += w
             right_seg[size+x] += w
             total_seg[size+x] += w
-        for j in range(size - 1, 0, -1):
-            seg[j] = seg[j*2] + seg[j*2+1]
-            left_seg[j] = max(left_seg[j*2], seg[j*2] + left_seg[j*2+1])
-            right_seg[j] = max(right_seg[j*2+1], right_seg[j*2] + seg[j*2+1])
-            total_seg[j] = max(max(total_seg[j*2], total_seg[j*2+1]), right_seg[j*2] + left_seg[j*2+1])
+            idx = (size+x) >> 1
+            while idx:
+                seg[idx] = seg[idx*2] + seg[idx*2+1]
+                left_seg[idx] = max(left_seg[idx*2], seg[idx*2] + left_seg[idx*2+1])
+                right_seg[idx] = max(right_seg[idx*2+1], right_seg[idx*2] + seg[idx*2+1])
+                total_seg[idx] = max(total_seg[idx*2], total_seg[idx*2+1], right_seg[idx*2] + left_seg[idx*2+1])
+                idx >>= 1
         result = max(result, total_seg[1])
 
 print(result)
