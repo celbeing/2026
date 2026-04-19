167bde932852ccc42f15238d49fa7711967eba35
전라남도교육지원청 <76736721+celbeing@users.noreply.github.com>
2026-04-20 01:18:36 +0900

    Update test.py

--- STUDY/test.py
+++ STUDY/test.py
@@ -1,29 +1,32 @@
-from collections import deque
+import sys
+input = sys.stdin.readline
 
-n = int(input())
-i,j = map(int, input().split())
-d = [(1,0),(0,1),(-1,0),(0,-1)]
-i -= 1; j -= 1
-mount = [list(map(int, input().split())) for _ in range(n)]
-INF = float('inf')
-cost = [[INF] * n for _ in range(n)]
-bfs = deque([(i,j)])
-cost[i][j] = 0
+n, m = map(int, input().split())
+head = [i for i in range(n + 1)]
+count = [1] * (n+1)
 
-res = INF
-while bfs:
-    x, y = bfs.popleft()
-    for dx, dy in d:
-        nx, ny = x + dx, y + dy
-        if 0 <= nx < n and 0 <= ny < n:
-            if mount[x][y] < mount[nx][ny]:
-                nc = cost[x][y] + mount[nx][ny] - mount[x][y]
-            else:
-                nc = cost[x][y] + (mount[x][y] - mount[nx][ny]) ** 2
-            if nc < cost[nx][ny]:
-                cost[nx][ny] = nc
-                bfs.append((nx, ny))
+def find(k):
+    t = k
+    while head[t] != t:
+        t = head[t]
+    head[k] = t
+    return t
 
-        else:
-            res = min(res, cost[x][y] + mount[x][y] ** 2)
-print(res)
+def union(a, b):
+    A = find(a)
+    B = find(b)
+    if A < B:
+        head[B] = A
+        count[A] += count[B]
+    elif A > B:
+        head[A] = B
+        count[B] += count[A]
+
+for _ in range(m):
+    q = list(map(int, input().split()))
+    if q[0] == 1:
+        x, y = q[1:]
+        union(x,y)
+    else:
+        x = q[1]
+        print(count[find(x)])
