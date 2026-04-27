5a393c0798354f0b5e146aa60f640c5989cf54ad
전라남도교육지원청 <76736721+celbeing@users.noreply.github.com>
2026-04-27 15:20:00 +0900

    Create 8571 택배 운송.py

---
+++ Jungol/Specialist/구간질의/8571 택배 운송.py
@@ -0,0 +1,51 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n, q = map(int, input().split())
+road = [dict() for _ in range(n+1)]
+robot = dict()
+for _ in range(n-1):
+    u, v, w = map(int, input().split())
+    road[u][v] = w
+    road[v][u] = w
+
+parent = [i for i in range(n+1)]
+depth = [-1] * (n+1)
+dist_from_1 = [0] * (n+1)
+branch_length = [0] * (n+1)
+branch_from = [i for i in range(n+1)]
+stem = set()
+
+depth[1] = 0
+bfs = deque([1])
+
+while bfs:
+    now = bfs.popleft()
+    for nxt in road[now]:
+        if depth[nxt] == -1:
+            parent[nxt] = now
+            depth[nxt] = depth[now]+1
+            dist_from_1 = dist_from_1[now]+road[now][nxt]
+            bfs.append(nxt)
+
+stem.add(n)
+idx = n
+while idx > 1:
+    idx = parent[idx]
+    stem.add(idx)
+    for nxt in road[idx]:
+        if not(nxt in stem or nxt != parent[idx]):
+            bfs.append(nxt)
+            while bfs:
+                
+
+
+
+for i in range(1,q+1):
+    query = list(map(int, input().split()))
+    if query[1] == 1:
+        a, b = query[1:]
+
+    else:
+        c = query[1]
