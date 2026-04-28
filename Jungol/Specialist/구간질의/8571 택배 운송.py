7a0f1fd21c3b4178b30d5829ac902d678276b990
전라남도교육지원청 <76736721+celbeing@users.noreply.github.com>
2026-04-29 00:30:45 +0900

    Update 8571 택배 운송.py

--- Jungol/Specialist/구간질의/8571 택배 운송.py
+++ Jungol/Specialist/구간질의/8571 택배 운송.py
@@ -49,7 +49,7 @@ point_set = set()
 robot = []
 for i in range(1,q+1):
     query = list(map(int, input().split()))
-    if query[1] == 1:
+    if query[0] == 1:
         a, b = query[1:]
         b -= branch_length[a]
         s, e = branch_from[a]-b, branch_from[a]+b
@@ -74,9 +74,43 @@ size = 1
 while size < l:
     size <<= 1
 
-cover_seg = [0] * size * 2
+cover_seg = [1] * size * 2
 count_seg = [0] * size * 2
 
+for i in range(1,size+l):
+    cover_seg[i] = 0
+
+def update(node):
+    if count_seg[node]:
+        cover_seg[node] = 1
+        node >>= 1
+        while node:
+            if cover_seg[node*2] == cover_seg[node*2+1] == 1:
+                cover_seg[node] = 1
+                node >>= 1
+            else:
+                cover_seg[node] = 0
+                break
+    else:
+        while node:
+            cover_seg[node] = 0
+            node >>= 1
+
 for w, i, j in robot:
-    i, j = i+size, j+size
-    
+    i, j = coor[i]+size, coor[j]+size
+    while i < j:
+        if i & 1:
+            count_seg[i] += w
+            update(i)
+            i += 1
+        i >>= 1
+        if not(j & 1):
+            count_seg[j] += w
+            update(j)
+            j -= 1
+        j >>= 1
+    if i == j:
+        count_seg[i] += w
+        update(i)
+
+    print('YES' if cover_seg[1] else 'NO')
