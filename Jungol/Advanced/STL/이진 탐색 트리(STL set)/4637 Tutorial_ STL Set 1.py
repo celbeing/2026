6db7d2bd5326bce2f35069dfb9f33ee3d231be6e
전라남도교육지원청 <76736721+celbeing@users.noreply.github.com>
2026-04-30 15:08:07 +0900

    Update 4637 Tutorial_ STL Set 1.py

--- Jungol/Advanced/STL/이진 탐색 트리(STL set)/4637 Tutorial_ STL Set 1.py
+++ Jungol/Advanced/STL/이진 탐색 트리(STL set)/4637 Tutorial_ STL Set 1.py
@@ -3,12 +3,23 @@
 import sys
 input = sys.stdin.readline
 
-s = set()
+s = dict()
+idx = 1
 for _ in range(int(input())):
     q, n = input().split()
     n = int(n)
     if q == 'i':
-        s.add(n)
+        if not(n in s):
+            s[n] = idx
+            idx += 1
     else:
-        s.discard(n)
-print(*sorted(list(s)))
+        if n in s:
+            del s[n]
+
+x = int(input())
+for a in s:
+    if x == s[a]:
+        print(s[a])
+        break
+else:
+    print('OVER')
