48b4a4792df0e420f83f8dee3615ec7b03ef2138
전라남도교육지원청 <76736721+celbeing@users.noreply.github.com>
2026-05-29 15:52:51 +0900

    Update 3032 능선.py

--- Jungol/3032 능선.py
+++ Jungol/3032 능선.py
@@ -1,15 +1,46 @@
 import sys
-from heapq import heappush, heappop
 from collections import deque
 input = sys.stdin.readline
 
+d = [(1,0),(0,1),(-1,0),(0,-1)]
 h, w = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(h)]
-
-hq = []
+graph = [[[] for _ in range(w)] for _ in range(h)]
+income = [[0] * w for _ in range(h)]
+site = [[0] * w for _ in range(h)]
+sink = []
 for i in range(h):
     for j in range(w):
-        heappush(hq, (-grid[i][j],i,j))
+        for dx, dy in d:
+            ni, nj = i+dx, j+dy
+            if 0<=ni<h and 0<=nj<w:
+                if grid[i][j] < grid[ni][nj]:
+                    graph[i][j].append((ni,nj))
+                    income[i][j] += 1
+            else: income[i][j] += 1
 
-# 물줄기 갈라졌다가 합쳐지는거 어떻게 판단하지?
-while hq
+for i in range(h):
+    for j in range(w):
+        if income[i][j] == 4:
+            sink.append((i, j))
+
+
+for i, j in sink:
+    bfs = deque()
+    bfs.append((i,j))
+    check = set()
+    while bfs:
+        x, y = bfs.popleft()
+        for nx, ny in graph[x][y]:
+            if (nx, ny) in check: continue
+            else:
+                site[nx][ny] += 1
+                check.add((nx, ny))
+                bfs.append((nx, ny))
+
+res = 0
+for i in range(h):
+    for j in range(w):
+        if site[i][j] > 1:
+            res += 1
+print(res)
