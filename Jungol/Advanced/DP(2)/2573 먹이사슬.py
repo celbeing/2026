b7e4283c6716f9564bb3f34a54a93afe010a7af2
전라남도교육지원청 <kimsd1983@naver.com>
2026-08-08 00:55:50 +0900

    Update 2573 먹이사슬.py

--- Jungol/Advanced/DP(2)/2573 먹이사슬.py
+++ Jungol/Advanced/DP(2)/2573 먹이사슬.py
@@ -1,54 +1,34 @@
+from bisect import bisect_left
+
 def solution():
     n = int(input())
-    cord = set()
     check = set()
     for _ in range(n):
         i, l, r = map(int, input().split())
-        cord.add(l); cord.add(r)
         if (l, r) in check: continue
         check.add((l, r))
-    cord_map = dict()
-    for i, c in enumerate(sorted(list(cord))):
-        cord_map[c] = i
-    animal = sorted(list(check), key=lambda x:(x[1], -x[0]))
-    n = len(animal)
-    for i in range(n):
-        animal[i] = (cord_map[animal[i][0]], cord_map[animal[i][1]])
+    animal = sorted(check, key=lambda x:(x[1], -x[0]))
+    del check
+    left = sorted(l for l, r in animal)
+    n = len(left)
 
-    k = len(cord)
-    size = 1
-    while size < k:
-        size <<= 1
-    seg = [0] * (size * 2)
+    fw = [0] * (n+1)
 
-    def update(i, x):
-        k = i+size
-        seg[k] = x
-        k >>= 1
-        while k:
-            seg[k] = max(seg[k*2], seg[k*2+1])
-            k >>= 1
+    def fw_update(i, x):
+        while i <= n:
+            fw[i] = max(fw[i], x)
+            i += i&-i
 
-    def query(l, r):
-        l, r = l+size, r+size
+    def fw_query(k):
         res = 0
-        while l < r:
-            if l & 1:
-                res = max(res, seg[l])
-                l += 1
-            if not(r & 1):
-                res = max(res, seg[r])
-                r -= 1
-            l >>= 1
-            r >>= 1
-        if l == r:
-            res = max(res, seg[l])
+        while k:
+            res = max(res, fw[k])
+            k -= k&-k
         return res
 
     for l, r in animal:
-        k = query(l, r)
-        update(l, k+1)
+        idx = n - bisect_left(left, l)
+        fw_update(idx, fw_query(idx)+1)
 
-    print(query(0, len(cord)-1))
-
+    print(fw_query(n))
 solution()
